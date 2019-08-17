a1ad34635bd533c0386d0d7b2d036fecbca48213
DobisPR <47193076+JMulligan87@users.noreply.github.com>
2019-08-16 20:44:13 -0400

    created package json

--- bamazon_DB.sql
+++ bamazon_DB.sql
@@ -5,7 +5,7 @@ CREATE DATABASE bamazon_DB;
 USE bamazon_DB;
 
 CREATE TABLE products (
-	item_id INT NOT NULL AUTO_INCREMENT,
+	item_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     product_name VARCHAR(50) NOT NULL,
     department_name VARCHAR(30) NOT NULL,
     price DECIMAL(10,4) NOT NULL,
