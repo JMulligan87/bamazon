d94ae5931c2bf56df2094141ad81827857fbf4cf
DobisPR <47193076+JMulligan87@users.noreply.github.com>
2019-08-16 20:20:29 -0400

    made some of the initial files

---
+++ bamazon_DB.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS bamazon_DB;
+
+CREATE DATABASE bamazon_DB;
+
+USE bamazon_DB;
+
+CREATE TABLE products (
+	item_id INT NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR(50) NOT NULL,
+    department_name VARCHAR(30) NOT NULL,
+    price DECIMAL(10,4) NOT NULL,
+    stock_quantity INT NOT NULL
+);
